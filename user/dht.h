f6ddeed4d0ab283fff49c6b5258bd20fffe6cca6
Thomas Zimmermann <bugs@vdm-design.de>
2015-12-06 12:56:34 +0100

    Add libraries for DHT11/22 humidity sensors and BMP180 pressure sensor

---
+++ user/dht.h
@@ -0,0 +1,22 @@
+#ifndef __dht_h__
+#define __dht_h__
+// https://github.com/mathew-hall/esp8266-dht
+
+enum sensor_type{
+	SENSOR_DHT11,SENSOR_DHT22
+};
+
+struct sensor_reading{
+	float temperature;
+	float humidity;
+	const char* source;
+	uint8_t sensor_id[16];
+	BOOL success;
+};
+
+
+void ICACHE_FLASH_ATTR DHT(void);
+struct sensor_reading * ICACHE_FLASH_ATTR readDHT(int force);
+void DHTInit(enum sensor_type, uint32_t polltime);
+
+#endif
